ba13c6cfca6739d5a024fcd7327d5a34a82d2a4f
Karlin Cruz <karlinkd010@gmail.com>
2022-05-06 16:41:28 -0500

    Update TransaccionServices.cs

--- Back_Caja_Sucursal_LATAM/Services/TransaccionServices.cs
+++ Back_Caja_Sucursal_LATAM/Services/TransaccionServices.cs
@@ -94,7 +94,7 @@ namespace Back_Caja_Sucursal_LATAM.Services
             }
         }
 
-        public Response getConsultaSaldo(Int64 saldo,Int64 noTarjeta, int noCajero)
+        public Response getConsultaSaldo(Int64 saldo,Int64 noTarjeta, Int64 noCajero)
         {
             Response respuesta = new Response();
             try
@@ -103,6 +103,7 @@ namespace Back_Caja_Sucursal_LATAM.Services
                 {
 
                     respuesta = db.Query<Response>("sp_getValidaSaldo", new { Saldo = saldo, NoTarjeta = noTarjeta, NoCajero= noCajero }, commandType: CommandType.StoredProcedure).FirstOrDefault();
+                    
                     if (respuesta == null)
                     {
                         return respuesta = new Response()
@@ -126,8 +127,93 @@ namespace Back_Caja_Sucursal_LATAM.Services
             }
         }
 
+        public Response insertTransaccion(Int64 saldo, Int64 noTarjeta, int noCajero)
+        {
+            Response result = new Response();
+            try
+            {
+                using (IDbConnection db = Connection)
+                {
+                    db.Open();
+                    result = db.Query<Response>("sp_InsertTransaccionRetiro", new { Saldo = saldo, NoTarjeta = noTarjeta, NoCajero= noCajero }, commandType: CommandType.StoredProcedure).FirstOrDefault();
+
+                    //if (result == null)
+                    //{
+                    //    return result = new Response()
+                    //    {
+                    //        Estatus = "Incorrecto",
+                    //        Mensaje = "Registro no se pudo guardar correctamente"
+                    //    };
+
+                    //}
+                    //if (result. == 0)
+                    //{
+                    //    return result = new Response()
+                    //    {
+                    //        Estatus = "Error al guardar",
+                    //        Mensaje = "Registro " + prod.Name + " ya existe en el base de datos!"
+                    //    };
+                    //}
+                    //if (result.Name != null && result.Creation != null)
+                    //{
+                    //    return result = new Response()
+                    //    {
+                    //        Name = "Correcto",
+                    //        Mensaje = "Registro " + response.Name + " guardado correctamente"
+                    //    };
+                    //}
+
+
+                    db.Close();
+
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return new Response()
+                {
+                    Estatus = "Error",
+                    Mensaje = ex.Message
+                };
+            }
+        }
+        public Response getConsultaSaldoCajero( Int64 noCajero)
+        {
+            Response result = new Response();
+            try
+            {
+                using (IDbConnection db = Connection)
+                {
+
+                    result = db.Query<Response>("sp_getConsultaSaldoCajero", new { NoCajero= noCajero }, commandType: CommandType.StoredProcedure).FirstOrDefault();
+                    if (respuesta == null)
+                    {
+                        return respuesta = new Response()
+                        {
+                            Estatus = "Incorrecto",
+                            Mensaje = "¡Hubo algún Error en el Servidor!"
+                        };
+
+                    }
+                    return result;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return new Response()
+                {
+                    Estatus = "Error",
+                    Mensaje = ex.Message
+                };
+            }
+        }
+
+
 
 
     }
-    
+
 }
