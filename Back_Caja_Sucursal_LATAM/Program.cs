60f49e4d296c9387d96a64a4c047a654d0251889
Karlin Cruz <karlinkd010@gmail.com>
2022-05-06 10:02:16 -0500

    Update Program.cs

--- Back_Caja_Sucursal_LATAM/Program.cs
+++ Back_Caja_Sucursal_LATAM/Program.cs
@@ -1,3 +1,6 @@
+using Back_Caja_Sucursal_LATAM.Interfaces;
+using Back_Caja_Sucursal_LATAM.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ITransaccionService, TransaccionServices>();
 
 var app = builder.Build();
 
