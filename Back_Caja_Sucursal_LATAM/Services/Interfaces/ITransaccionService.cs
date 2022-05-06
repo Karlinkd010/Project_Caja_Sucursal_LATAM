2c9783db1e8238098cd2ea3763cf1348e7a32e75
Karlin Cruz <karlinkd010@gmail.com>
2022-05-06 16:41:35 -0500

    Update ITransaccionService.cs

--- Back_Caja_Sucursal_LATAM/Services/Interfaces/ITransaccionService.cs
+++ Back_Caja_Sucursal_LATAM/Services/Interfaces/ITransaccionService.cs
@@ -4,16 +4,11 @@ namespace Back_Caja_Sucursal_LATAM.Interfaces
 {
     public interface ITransaccionService
     {
-        //public List<Product> getProducts();
-        //public Product getProduct(int Id);
-        //public Response insertProduct(Product prod);
-        //public Response updateProduct(Product prod);
-        //public Response deleteProduct(int id);
-
         public Response getTarjeta(Int64 NoTarjeta);
         public Response getNip(Int64 noTarjeta, int noNip);
-        public Response getConsultaSaldo(Int64 saldo, Int64 noTarjeta, int noCajero);
-
+        public Response getConsultaSaldoCajero(Int64 noCajero);
+        public Response getConsultaSaldo(Int64 saldo, Int64 noTarjeta, Int64 noCajero);
+        public Response insertTransaccion(Int64 saldo, Int64 noTarjeta, int noCajero);
 
     }
 }
