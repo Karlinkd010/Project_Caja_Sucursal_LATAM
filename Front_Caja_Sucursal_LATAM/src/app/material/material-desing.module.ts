292a71ef981328d514606264adaf0e4c895e7eee
Karlin Cruz <karlinkd010@gmail.com>
2022-05-13 09:16:36 -0500

    Create material-desing.module.ts

---
+++ Front_Caja_Sucursal_LATAM/src/app/material/material-desing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule} from '@angular/material/list';
+import { MatCardModule} from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatSelectModule} from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { MatProgressBarModule} from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { MatGridListModule} from '@angular/material/grid-list';
+import { MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatDialogModule} from '@angular/material/dialog';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [],
+  exports: [
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatSidenavModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatInputModule ,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
+    MatSnackBarModule
+    
+  ]
+})
+export class MaterialDesingModule { }
