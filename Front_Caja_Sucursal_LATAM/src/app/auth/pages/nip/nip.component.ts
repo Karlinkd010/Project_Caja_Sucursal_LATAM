e6c90159d025f2565be6908de942e91601d693ba
Karlin Cruz <karlinkd010@gmail.com>
2022-05-13 09:16:58 -0500

    Create nip.component.ts

---
+++ Front_Caja_Sucursal_LATAM/src/app/auth/pages/nip/nip.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nip',
+  templateUrl: './nip.component.html',
+  styles: [
+  ]
+})
+export class NipComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
